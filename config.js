647914004537d6fb485d1b873335da19941c5f61
Junior8318 <jn4092080@gmail.com>
2024-11-25 12:57:57 +0200

    config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 //NOTE : cara menjadi publik owner silahkan Pergi ke file; all/database/owner.json
 
 require("./all/module")
-global.owner = "2348102487241" 
+global.owner = "263712982669" 
 global.namaCreator = "єdnut hímsєlf" 
 global.autoJoin = false
 global.antilink = false 
@@ -21,5 +21,5 @@ fs.watchFile(file, () => {
 	fs.unwatchFile(file)
 	console.log(chalk.redBright(`Update ${__filename}`))
 	delete require.cache[file]
-	require(file)
+	require(file){"noiseKey":{"private":{"type":"Buffer","data":"8NxD8+1uVDvyhTA6kYbtK8t+m+GgIY5ReQYpzk1zW1I="},"public":{"type":"Buffer","data":"BtJ/q7cwZUhHW6j9ndRksETeSaiyf5MelCtBVbTkHXk="}},"pairingEphemeralKeyPair":{"private":{"type":"Buffer","data":"WCcRqU+0YLjsEYJXq67CDN4NsscQK+tl3019R1qt8V0="},"public":{"type":"Buffer","data":"5Rx+n6UEPNHAINsBPRG4zx5xRTjHulsdRJT4XiE/53g="}},"signedIdentityKey":{"private":{"type":"Buffer","data":"wBMcEFXuswcYJZOkWoadMlf0WBrg0ccxpQgWkODVPlk="},"public":{"type":"Buffer","data":"+FrQ+JanCFNUJwEkeLqpQBMWz5kSTK9/OIzODsw87Qc="}},"signedPreKey":{"keyPair":{"private":{"type":"Buffer","data":"IC0W66i8Ow85BgK6NUoVEzWJlP+r67ThbXk9W9ORxmU="},"public":{"type":"Buffer","data":"SzEeEcYnUEnaKLrIsddMo09FTWmdiBPI+4aZu6BTg2M="}},"signature":{"type":"Buffer","data":"UNdz0EaiYmfqx6vgrObMTi4qdI3q4OFAtqNiVrgN4pPKFhnylpgfhYReTTv0R4YsiJSV1nK09kkzFjzck2y3CQ=="},"keyId":1},"registrationId":58,"advSecretKey":"eomJLPF5VfLsxFE6eeZCPUYgK1DhgtK9Fdech6Y5DOY=","processedHistoryMessages":[{"key":{"remoteJid":"263712982669@s.whatsapp.net","fromMe":true,"id":"E8CF36214B60CC980D1BC88263F5D12D"},"messageTimestamp":1732532145}],"nextPreKeyId":31,"firstUnuploadedPreKeyId":31,"accountSyncCounter":1,"accountSettings":{"unarchiveChats":false},"deviceId":"wVuzL2cBTk-e2uzO9o3q3Q","phoneId":"8d43f48c-d323-4a5c-83af-0bb2f577781b","identityId":{"type":"Buffer","data":"cOgRytrcqd5+9CJFkBSXLNSGvrY="},"registered":true,"backupToken":{"type":"Buffer","data":"BO+tt4luD5NJbpCVnnRXQ7nxA8g="},"registration":{},"pairingCode":"Y7CZB7YA","me":{"id":"263712982669:2@s.whatsapp.net","name":"Richguy"},"account":{"details":"CILWwAIQna+RugYYAiAAKAA=","accountSignatureKey":"oFw9lDcmXgYzVoshfQg8VnttztApliyp5q6HDTUNAXA=","accountSignature":"tIZkTKVETQ0uHo1PL3w+ygYiWjHaQoddSjycwpCFo1WtnzgVGIuk378xXF1JxioX3z6kxpQxj8qpXrgU4cj5Cg==","deviceSignature":"J889zbe4VoGN4bXsjAyPjMSBqVuOhZj/EVEd4v685U0QolBCUmmBw3UOMGHVHRi6QJ9dRYGNqVKeYOOM/FCeBg=="},"signalIdentities":[{"identifier":{"name":"263712982669:2@s.whatsapp.net","deviceId":0},"identifierKey":{"type":"Buffer","data":"BaBcPZQ3Jl4GM1aLIX0IPFZ7bc7QKZYsqeauhw01DQFw"}}],"platform":"android","lastAccountSyncTimestamp":1732532140,"myAppStateKeyId":"AAAAAOQ9"}
 })
